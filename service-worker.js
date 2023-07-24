7772c0d8f7acec4318014dc16cb4056e9e2d379a
LavrentySeverin <Severin.lavr@yandex.ru>
2023-07-24 22:05:03 +0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// Установка сервисного работника
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+	    '/',
+        '/photjpg.jpg',
+		'/site.html',
+        '/sill.css',
+        '/chan55.jpg',
+        '/chan75.jpg'
+      ]);
+    })
+  );
+});
+
+// Активация сервисного работника
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          // Удаляем старые кэши, оставляя только текущий кэш 'my-cache'
+          return cacheName !== 'my-cache';
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Запрос на сети и обновление кеша
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
